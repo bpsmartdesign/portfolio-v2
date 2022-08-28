4226775bd461c59f38b220afeca59f8e2cd5b687
BIYA Paul <paul@workerly.io>
2022-08-28 11:57:42 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  //TODO load main css depending on current themeMode
+  // TODO load main css depending on current themeMode
   css: [
     '~/assets/css/animate.css',
     '~/assets/scss/main.scss',
